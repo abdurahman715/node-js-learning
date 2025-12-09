15547bbfd6ee57821e2bcfe9e7aa98db6b9525a7
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2025-12-09 13:01:18 +0530

    Created first server

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+app.get("/products", (req, res) => {
+  res.send("Products page");
+});
+app.listen(7777, () => {
+  console.log("Server started running on port 7777");
+});
