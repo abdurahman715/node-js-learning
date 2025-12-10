1e5240ea428c2bf33b60f401badb08186de21727
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2025-12-10 10:22:00 +0530

    ....

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Home page");
-});
-app.post("/form", (req, res) => {
-  res.send("Data saved successfully");
-});
-app.delete("/data", (req, res) => {
-  res.send("Data deleted successfully");
-});
 app.listen(7777, () => {
   console.log("Server started running on port 7777");
 });
