fc0c8ddf177dbe15fa2c31ea052de853feeafd95
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2025-12-10 11:48:18 +0530

    Basi route handlers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
-app.get.apply.apply.apply.apply.app.listen(7777, () => {
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Route one");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route two");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route 3");
+    res.send("3rd response");
+  }
+);
+app.listen(7777, () => {
   console.log("Server started running on port 7777");
 });
