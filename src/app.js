e01ef7a12836b5c0cd2309129f558e514a5d9f32
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2025-12-14 13:38:30 +0530

    Dynamic signup API

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ const express = require("express");
 const connectDB = require("./config/database.js");
 const app = express();
 const User = require("./models/user.js");
+app.use(express.json());
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Muhammed",
-    lastName: "Abdurahman ck",
-    email: "abdurahmanck715@gmail.com",
-    password: 12345,
-    gender: "Male",
-  });
+  // console.log(req.body);
+  // const user = new User({
+  //   firstName: "Muhammed",
+  //   lastName: "Abdurahman ck",
+  //   email: "abdurahmanck715@gmail.com",
+  //   password: 12345,
+  //   gender: "Male",
+  // });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User added successfully");
