afe1e0849f3c127bcad6a8986dcd2a39edfbd760
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2025-12-14 09:24:14 +0530

    Removed

--- database.js
+++ database.js
@@ -1,2 +1,15 @@
-require("mongodb");
-const URI = "mongodb+srv://Muhammed:kl53j5868@cluster0.m6axrlu.mongodb.net/";
+const { MongoClient } = require("mongodb");
+const url = "mongodb+srv://Muhammed:kl53j5868@cluster0.m6axrlu.mongodb.net/";
+const client = new MongoClient(url);
+const dbName = "helloworld";
+async function main() {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("user");
+  return "done";
+}
+main()
+  .then(console.log)
+  .catch(console.err)
+  .finally(() => client.close());
