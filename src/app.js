d93e6247ab113708994ace7a89b3e8e42a5a10c3
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2025-12-16 18:15:36 +0530

    Implemented lot of features

--- src/app.js
+++ src/app.js
@@ -40,11 +40,9 @@ app.post("/login", async (req, res) => {
     if (!user) {
       throw new Error("Invalid credentials");
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await user.validatePassword(password);
     if (isPasswordValid) {
-      const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
-        expiresIn: "1d",
-      });
+      const token = await user.getJWT();
       console.log(token);
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
